2b213efe549bad5b9fb6227a72926585df603f9d
Wanasak <u510610433@gmail.com>
2017-08-01 00:33:13 +0700

    add validation

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-form',
@@ -9,8 +9,11 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class SignupFormComponent {
 
   form = new FormGroup({
-    email: new FormControl(),
-    password: new FormControl()
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
   });
 
+  get email() {
+    return this.form.get('email');
+  }
 }
